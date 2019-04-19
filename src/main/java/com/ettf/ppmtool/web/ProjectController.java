416e5d6bd7a90895dd462222d2f4c5baae6a53e4
etff <wish2rich@gmail.com>
2019-04-19 23:18:57 +0900

    add error service

--- src/main/java/com/ettf/ppmtool/web/ProjectController.java
+++ src/main/java/com/ettf/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.ettf.ppmtool.web;
 
 import com.ettf.ppmtool.domain.Project;
+import com.ettf.ppmtool.services.MapValidationErrorService;
 import com.ettf.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,18 +25,16 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if(result.hasErrors()) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
 
-            Map<String, String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        if(errorMap != null){
+            return errorMap;
         }
 
         Project project1 = projectService.saveOrUpdateProject(project);
