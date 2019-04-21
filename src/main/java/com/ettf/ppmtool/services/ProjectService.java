ed6cc9720c228794577e77357cb0e0649b429836
etff <wish2rich@gmail.com>
2019-04-21 23:55:17 +0900

    add duplicate validate

--- src/main/java/com/ettf/ppmtool/services/ProjectService.java
+++ src/main/java/com/ettf/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.ettf.ppmtool.services;
 
 import com.ettf.ppmtool.domain.Project;
+import com.ettf.ppmtool.exceptions.ProjectIdException;
 import com.ettf.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifer(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already existed ");
+        }
+
+
     }
 }
