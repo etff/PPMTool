fce4574fb8e5de97dad9072961b510ada6a1f4ce
etff <wish2rich@gmail.com>
2019-05-31 02:03:19 +0900

    add html, css files

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Welcome to dashboard</h1>
-                <ProjectItem/>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<div className="projects">
+					<div className="container">
+						<div className="row">
+							<div className="col-md-12">
+								<h1 className="display-4 text-center">프로젝트 관리</h1>
+								<br />
+								<br />
+								<hr />
+
+								<ProjectItem />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Dashboard;
--- client/src/components/Project/ProjectItem.js
+++ client/src/components/Project/ProjectItem.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class ProjectItem extends Component {
-    render() {
-        return (
-            <h1>Project Item</h1>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<div className="container">
+					<div className="card card-body bg-light mb-3">
+						<div className="row">
+							<div className="col-2">
+								<span className="mx-auto">REACT</span>
+							</div>
+
+							<div className="col-lg-6 col-md-4 col-8">
+								<h3>Spring Boot/ React </h3>
+
+								<p>스프링 부트 + 리액트 연동</p>
+							</div>
+
+							<div className="col-md-4 d-none d-lg-block">
+								<ul className="list-group">
+									<a href="/">
+										<li className="list-group-item board">
+											<i className="fas fa-chalkboard"> 게시판 </i>
+										</li>
+									</a>
+
+									<a href="/">
+										<li className="list-group-item update">
+											<i className="fas fa-check"> 업데이트</i>
+										</li>
+									</a>
+
+									<a href="/">
+										<li className="list-group-item delete">
+											<i className="fas fa-trash pr-1"> 삭제</i>
+										</li>
+									</a>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ProjectItem;
